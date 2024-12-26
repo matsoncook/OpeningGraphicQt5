0697c8efd0cda97f8d2a203309853243764937e8
Mark Cook <markcook2001@gmail.com>
2024-12-26 16:39:13 +1300

    sixth

--- src/glowing_circle_animations/glowing_circle_animator.py
+++ src/glowing_circle_animations/glowing_circle_animator.py
@@ -35,10 +35,10 @@ class MoveGenerally(Animator):
         return
 
 class MoveRight_1(Animator):
-    def __init__(self,glowing_circle):
+    def __init__(self,glowing_circle,velocity = 1.0):
         super().__init__()
         self.glowing_circle = glowing_circle
-        self.velocity = 1
+        self.velocity = velocity
         self.direction = 1
         self.x = -1
         self.reset()
@@ -56,30 +56,31 @@ class MoveRight_1(Animator):
             self.direction *= -1
             finish = True
 
-        c_x += (0.01 * self.direction)
+        #c_x += (0.01 * self.direction)
 
 
         self.glowing_circle.circle_center[0] = c_x
         self.x = c_x
         return finish
 
-    # def next_animation(self) -> "Animator":
-    #     return self
+    def next_animation(self) -> "Animator":
+        return MoveLeft_1(self.glowing_circle,0.5)
 
     def reset(self):
         self.x = -1
         return
 
 class MoveLeft_1(Animator):
-    def __init__(self, glowing_circle):
+    def __init__(self, glowing_circle,velocity = 1.0):
         super().__init__()
         self.glowing_circle = glowing_circle
-        self.velocity = 1
+        self.velocity = velocity
         self.direction = -1
         self.x = 1
         self.reset()
 
     def do_update(self, elapsed_time_secs):
+        finish  = False
         distance_elapse = self.velocity * elapsed_time_secs
 
         # c_x = self.glowing_circle.circle_center[0]
@@ -89,15 +90,16 @@ class MoveLeft_1(Animator):
 
         if c_x >= 1.0 or c_x <= -1.0:
             self.direction *= -1
+            finish = True
 
-        c_x += (0.01 * self.direction)
+
 
         self.glowing_circle.circle_center[0] = c_x
         self.x = c_x
-        return False
+        return finish
 
     def next_animation(self) -> "Animator":
-        return None
+        return MoveRight_1(self.glowing_circle,0.25)
 
     def reset(self):
         self.x = 1
